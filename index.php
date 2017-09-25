3503abcee1f42c4dade9012f9ce936aa15adae44
kayevalle <kayevalle04@gmail.com>
2017-09-25 22:21:37 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/docs/3.3/favicon.ico">
+
+    <title>West Palawan Premiere</title>
+    <link rel="icon" href="images/wpp3.png">
+    <link rel="stylesheet" href="css/navbar.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="css/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+
+    <style type="text/css">
+
+    </style>
+
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div >
+
+        <nav class="navbar navbar-default navbar-static-top" style="opacity: .9;">
+          <div class="container">
+            <div class="col-md-8 col-md-offset-2 vertical-center" >
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#" style="padding-top:4px;padding-right:0px;">
+                <div class="">
+                    <img src="images/wpp4.png" width="150">
+                    <!-- <img src="images/wpptext2.png" width="100"> -->
+                </div>
+                </a>
+              </div>
+              <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                  <!-- <li class="dropdown">
+                        <button class="dropbtn">Dropdown</button>
+                        <div class="dropdown-content">
+                          <a href="#">Link 1</a>
+                          <a href="#">Link 2</a>
+                          <a href="#">Link 3</a>
+                        </div>
+                  </li> -->
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-content" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
+                  
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                      </ul>
+                  
+                  </li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#contact">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+      </div>
+    </div>
+
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <img class="first-slide" src="images/lowresolution paul quiambao batch 1 (17).jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="images/lowresolution paul quiambao batch 1 (20).jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="third-slide" src="images/lowresolution paul quiambao batch 1 (27).jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
+    <div class="overlay">
+      <video width="100%" controls autoplay>
+        <source src="videos/small.mp4" type="video/mp4">
+      </video>
+    </div>
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="col-xs-12" style="margin-bottom: 20px">
+        <h3 id="projectstitle">PROJECTS</h3>
+      </div>
+      <div class="row" style="margin:10px">
+          <div class="col-sm-6 col-md-3 projectslist" id="projects" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="Some content">
+            <div class="image-projects-container">
+              <div class="projects-container">
+                <a href="#">
+                  <span class="projects-caption">
+                    <h3>Lazuli</h3>
+                  </span>
+                  <img src="images/lowresolution paul quiambao batch 1 (19).jpg" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3" id="projects">
+            <div class="image-projects-container">
+              <div class="projects-container">
+                <a href="#">
+                  <span class="projects-caption">
+                    <h3>Sapphire Sands</h3>
+                  </span>
+                  <img src="images/lowresolution paul quiambao batch 1 (7).jpg" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3" id="projects">
+            <div class="image-projects-container">
+              <div class="projects-container">
+                <a href="#">
+                  <span class="projects-caption">
+                    <h3>Diamond Cave</h3>
+                  </span>
+                  <img src="images/lowresolution paul quiambao batch 1 (20).jpg" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3" id="projects">
+            <div class="image-projects-container">
+              <div class="projects-container">
+                <a href="#">
+                  <span class="projects-caption">
+                    <h3>Midlands</h3>
+                  </span>
+                  <img src="images/lowresolution paul quiambao batch 1 (26).jpg" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+      </div><!-- /.row -->
+
+      </div><!-- /.container -->
+
+      <div class="container-fluid" style="overflow: hidden">
+        <div class="row">
+          <div class="col-sm-6 col-md-3 panorama-container">
+            
+            <iframe src="https://360player.io/p/rjhRh7/" frameborder="0"  data-token="rjhRh7"></iframe>
+          </div>
+        </div>
+      </div>  
+
+    <div class="container">
+      <!-- FOOTER -->
+      <footer>
+        <!-- <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>© 2016 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p> -->
+        <div class="footer-desc-row row">
+          <div class="footer-desc col-xs-12 col-sm-4 col-md-4">
+            <p style="border-bottom: medium solid grey"><strong>ABOUT US</strong></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu urna sit amet mi tempor fringilla ut et mi. Etiam augue odio, hendrerit consectetur erat eget, blandit volutpat sapien. Suspendisse potenti. Maecenas at ullamcorper risus. Duis ornare nulla massa. Cras suscipit quam eu libero commodo laoreet. Donec condimentum consectetur mollis. Proin consequat tempus risus eget consectetur. Duis euismod lacus vitae tortor pharetra cursus. Sed eget aliquet orci.</p>
+          </div>    
+          <div class="footer-desc col-xs-12 col-sm-4 col-md-4">
+            <p style="border-bottom: medium solid grey"><strong>ABOUT US</strong></p>
+            <p>Praesent faucibus leo ac tellus finibus, ac dictum tellus pellentesque. Suspendisse porta purus ut enim varius volutpat. Integer blandit mi eget erat mattis, et laoreet lorem elementum. In ultricies lectus nisi, a cursus neque molestie ut. Nam a luctus justo. Nulla facilisis, ante quis congue semper, justo eros porta nibh, in aliquet eros arcu ac mauris. Pellentesque maximus rhoncus orci eget pulvinar. In ultrices in felis vel egestas. Ut luctus risus eu leo congue dignissim.</p>
+          </div>    
+          <div class="footer-desc col-xs-12 col-sm-4 col-md-4">
+            <p style="border-bottom: medium solid grey"><strong>CONTACT US</strong></p>
+            <p>Cras in nibh tellus. Praesent in tellus tortor. Ut urna neque, mattis nec ante sit amet, malesuada laoreet nibh. Ut vulputate mattis nulla, vitae congue felis luctus sit amet. Donec ullamcorper ipsum vitae purus posuere, nec ornare nisl posuere. Curabitur iaculis tristique risus at imperdiet. Aliquam non tortor in nisi efficitur rutrum nec ac odio.</p>
+            <ul class="social-media" >
+              <li><i class="fa fa-facebook-official"></i></li>
+              <li><i class="fa fa-twitter-square"></i></li>
+              <li><i class="fa fa-instagram"></i></li>
+              <li><i class="fa fa-vimeo-square"></i></li>
+            </ul>
+          </div>    
+        </div>
+      </footer>
+      
+      <hr class="featurette-divider">
+  </div>
+    
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <script src="js/js.js"></script>
+  
+
+<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg></body></html>
